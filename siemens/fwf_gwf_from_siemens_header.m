4806d0832969bef40e77fbb87b0117dd0ad18241
Filip Szczepankiewicz <filip.szczepankiewicz@gmail.com>
2018-12-20 15:06:32 +0100

    Update fwf_gwf_from_siemens_header.m

--- siemens/fwf_gwf_from_siemens_header.m
+++ siemens/fwf_gwf_from_siemens_header.m
@@ -9,7 +9,7 @@ function [gwf, rf, dt] = fwf_gwf_from_siemens_header(h)
 % https://se.mathworks.com/matlabcentral/fileexchange/42997-xiangruili-dicm2nii
 %
 % Dicom header must be modified according to the conventions of the 
-% Free Waveform Encoding (FWF) sequence, and only works for sequence
+% Free Waveform Encoding (FWF) sequence, and only works for FWF sequence
 % versions 1.12 and later.
 
 csa           = fwf_csa_from_siemens_header(h);
