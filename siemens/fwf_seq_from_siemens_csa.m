0919385d660d0982c5bf2051e166a1d81170439d
Filip Szczepankiewicz <filip.szczepankiewicz@gmail.com>
2019-08-27 13:40:28 +0200

    Update fwf_seq_from_siemens_csa.m
    Replaced erase with strrep for better compatibility

--- siemens/fwf_seq_from_siemens_csa.m
+++ siemens/fwf_seq_from_siemens_csa.m
@@ -85,7 +85,7 @@ for i = 1:size(str_list, 1)
     
     if strcmp('str', str_list{i,3})
         val = sscanf(csa(ind2:end), '%s', 1);
-        val = erase(val,'""');
+        val = strrep(val, '""', '');
         
         if strcmp('sWipMemBlock.tFree	 = 	', str_list{i,1})
             val = fwf_b64_to_data(val);
