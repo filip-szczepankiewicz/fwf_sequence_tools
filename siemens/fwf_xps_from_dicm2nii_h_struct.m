8f3f61c383b3a6fc7c404e6d682719b32a334a2d
Filip Szczepankiewicz <filip.szczepankiewicz@gmail.com>
2021-02-22 15:17:24 +0100

    Update fwf_xps_from_dicm2nii_h_struct.m
    Updated input handling so that string and char is accepted... this is horrible and must be improved.

--- siemens/fwf_xps_from_dicm2nii_h_struct.m
+++ siemens/fwf_xps_from_dicm2nii_h_struct.m
@@ -1,20 +1,36 @@
 function xps_l = fwf_xps_from_dicm2nii_h_struct(h, o_dir)
 % function xps_l = fwf_xps_from_dicm2nii_h_struct(h)
-
+% 
 % Code written to be compatible with output from dicm2nii by
 % Xiangrui Li (xiangrui.li@gmail.com)
 % http://www.mathworks.com/matlabcentral/fileexchange/42997
+%
+% h can be header structure or path to a header .mat file.
+
+if nargin < 2
+    o_dir = []; % if empty it will not be saved.
+else
+    if isstring(h)
+        h = convertStringsToChars(h);
+    end
+       
+    if ischar(h) && isnumeric(o_dir)
+        if o_dir > 0
+            o_dir = [fileparts(h) filesep];
+        else
+            o_dir = [];
+        end
+    end
+end
 
 if ~isstruct(h)
     h = load(h);
     h = h.h;
 end
 
-fn_l = fieldnames(h);
 
-if nargin < 2
-    o_dir = [];
-end
+
+fn_l = fieldnames(h);
 
 for i = 1:numel(fn_l)
     
@@ -24,8 +40,9 @@ for i = 1:numel(fn_l)
     
     try
         xps = fwf_xps_from_siemens_hdr(h.(fn));
-    catch
+    catch me
         warning(['Failed to create xps for ' fn]);
+        disp(me.message)
         continue
     end
     
@@ -36,3 +53,5 @@ for i = 1:numel(fn_l)
         save(xps_fn,  'xps')
     end
 end
+
+
