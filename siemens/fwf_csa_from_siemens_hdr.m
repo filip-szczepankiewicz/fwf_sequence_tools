c53f6e2b58e8bfec0fe76bfc53cf813dfe0ab7c7
Filip Szczepankiewicz <filip.szczepankiewicz@gmail.com>
2021-02-22 15:16:36 +0100

    Update fwf_csa_from_siemens_hdr.m
    Fixed issue with Edmonton data having stored the CSA in a differnt place. This hack is temporary until header extraction can solve this at an earlier step.

--- siemens/fwf_csa_from_siemens_hdr.m
+++ siemens/fwf_csa_from_siemens_hdr.m
@@ -4,5 +4,19 @@ function csa = fwf_csa_from_siemens_hdr(h)
 % Brigham and Women's Hospital, Harvard Medical School, Boston, MA, USA
 % Lund University, Lund, Sweden
 
-csa = h.CSASeriesHeaderInfo.MrPhoenixProtocol;
-  
+try
+    csa = h.CSASeriesHeaderInfo.MrPhoenixProtocol;
+    return
+catch
+    warning('Regular CSA structure was not found!')
+end
+
+% This is temporary prototype code until dicm2nii figures out how to read
+% multiple CSA fields.
+try
+    csa = char(h.CSASeriesHeaderInfoX(:)');
+    disp('Reading irregular CSA! FIXME!')
+    return
+catch
+    error('Could not read CSA')
+end
