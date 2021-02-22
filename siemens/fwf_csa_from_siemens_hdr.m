3d4ba0108ca1fdcec1c91df463b7921f2e95839a
Filip Szczepankiewicz <filip.szczepankiewicz@gmail.com>
2021-02-22 20:40:47 +0100

    Update fwf_csa_from_siemens_hdr.m
    Issue with CSA series info was resolved. See https://github.com/xiangruili/dicm2nii/issues/35

--- siemens/fwf_csa_from_siemens_hdr.m
+++ siemens/fwf_csa_from_siemens_hdr.m
@@ -3,20 +3,7 @@ function csa = fwf_csa_from_siemens_hdr(h)
 % By Filip Szczepankiewicz
 % Brigham and Women's Hospital, Harvard Medical School, Boston, MA, USA
 % Lund University, Lund, Sweden
+% 
+% Based on the dicom header reader at https://github.com/xiangruili/dicm2nii
 
-try
-    csa = h.CSASeriesHeaderInfo.MrPhoenixProtocol;
-    return
-catch
-    warning('Regular CSA structure was not found!')
-end
-
-% This is temporary prototype code until dicm2nii figures out how to read
-% multiple CSA fields.
-try
-    csa = char(h.CSASeriesHeaderInfoX(:)');
-    disp('Reading irregular CSA! FIXME!')
-    return
-catch
-    error('Could not read CSA')
-end
+csa = h.CSASeriesHeaderInfo.MrPhoenixProtocol;
