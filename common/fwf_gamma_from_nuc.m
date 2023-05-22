2c670706e49966e5c9690aecdaab88e17f78b955
FilipSz <filip.szczepankiewicz@gmail.com>
2023-05-22 17:47:52 +0200

    restructuring

--- common/fwf_gamma_from_nuc.m
+++ common/fwf_gamma_from_nuc.m
@@ -1,6 +1,10 @@
 function gamma = fwf_gamma_from_nuc(str)
 % function gamma = fwf_gamma_from_nuc(str)
 
+if nargin < 1 || isempty(str)
+    str = '1H';
+end
+
 switch str
     case '1H'
         gamma = 2.6751e+08;
