5b079cf3736236ec02b98350afcaaf5c0f97b55d
FilipSz <filip.szczepankiewicz@gmail.com>
2023-08-23 15:21:22 +0200

    Update fwf_bin2gwf_siemens.m

--- siemens/fwf_bin2gwf_siemens.m
+++ siemens/fwf_bin2gwf_siemens.m
@@ -4,10 +4,15 @@ function GWF = fwf_bin2gwf_siemens(bin_fn)
 
 fileID = fopen(bin_fn, 'r');
 
+% Read meta data
+% ver = fread(fileID, 1, 'single');
+% sha = fread(fileID, X, 'undecided');
+
 n_U = fread(fileID, 1, 'int32');
 n_A = fread(fileID, 1, 'int32');
 n_B = fread(fileID, 1, 'int32');
 
+% Read waveforms
 gn_A(:,1) = fread(fileID, n_A*n_U, 'single');
 gn_A(:,2) = fread(fileID, n_A*n_U, 'single');
 gn_A(:,3) = fread(fileID, n_A*n_U, 'single');
@@ -17,9 +22,11 @@ gn_B(:,3) = fread(fileID, n_B*n_U, 'single');
 
 fclose(fileID);
 
+% Organize waveforms
 gn_A = permute(reshape(gn_A, n_A, n_U, 3), [1 3 2]);
 gn_B = permute(reshape(gn_B, n_B, n_U, 3), [1 3 2]);
 
+GWF = cell(2,n_U);
 for c = 1:n_U
     GWF{1,c} = gn_A(:,:,c);
     GWF{2,c} = gn_B(:,:,c);
