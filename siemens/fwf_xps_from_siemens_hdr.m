80712e64647c0ce913c90b8c4c6214b41abff594
FilipSz <filip.szczepankiewicz@gmail.com>
2023-11-23 12:29:55 +0100

    Update fwf_xps_from_siemens_hdr.m

--- siemens/fwf_xps_from_siemens_hdr.m
+++ siemens/fwf_xps_from_siemens_hdr.m
@@ -12,17 +12,11 @@ function xps = fwf_xps_from_siemens_hdr(hdr)
 % https://github.com/xiangruili/dicm2nii
 
 % This function depends a lot on the pulse sequence version
-
 [gwfc, rfc, dtc] = fwf_gwf_list_from_siemens_hdr(hdr);
-gamma            = fwf_gamma_from_nuc(fwf_nuc_from_siemens_hdr(hdr));
 
-btl              = fwf_gwfc_to_btens(gwfc, rfc, dtc, gamma);
-
-xps           = mdm_xps_from_bt(btl);
-xps.te        = ones(xps.n, 1) * hdr.EchoTime /1000;
-xps.tr        = ones(xps.n, 1) * hdr.RepetitionTime /1000;
-% xps.wf_ind    = ind;
+xps              = fwf_xps_from_gwfl(gwfc, rfc, dtc);
+xps.te           = ones(xps.n, 1) * hdr.EchoTime /1000;
+xps.tr           = ones(xps.n, 1) * hdr.RepetitionTime /1000;
 
 %% WIP
 % change calculation of b to be native to csa and seq info and not rely on bval/bvec
-% replace most of this with fwf_xps_from_gwfl
