971de9628c7ddbfe6b920469561d3bd0de7ea147
FilipSz <filip.szczepankiewicz@gmail.com>
2024-06-18 10:37:23 +0200

    fixes

--- common/fwf_xps_from_gwfl.m
+++ common/fwf_xps_from_gwfl.m
@@ -1,15 +1,15 @@
-function xps = fwf_xps_from_gwfl(gwfl, rfl, dtl, gamma)
-% function xps = fwf_xps_from_gwfl(gwfl, rfl, dtl, gamma)
+function xps = fwf_xps_from_gwfl(gwfl, rfl, dtl, gamma_H1)
+% function xps = fwf_xps_from_gwfl(gwfl, rfl, dtl, gamma_H1)
 
 if nargin < 4
-    gamma = fwf_gamma_from_nuc();
+    gamma_H1 = fwf_gamma_from_nuc();
 end
 
 n_vols = numel(gwfl);
 
 bt     = zeros(n_vols, 6);
 mt     = zeros(n_vols, 6);
-% gamma  = zeros(n_vols, 3);
+gamma  = zeros(n_vols, 3);
 gMom_0 = zeros(n_vols, 3);
 gMom_1 = zeros(n_vols, 3);
 gMom_2 = zeros(n_vols, 3);
@@ -29,15 +29,15 @@ for i = 1:n_vols
     qeff = cumsum(geff, 1) * dt;
     
     % B and M tensors
-    M           = gamma^2 * (geff' * geff) * dt; % bV_omega in Nilsson et al (2017) NMR Biomed, Eq. 24
-    B           = gamma^2 * (qeff' * qeff) * dt;
+    M           = gamma_H1^2 * (geff' * geff) * dt; % bV_omega in Nilsson et al (2017) NMR Biomed, Eq. 24
+    B           = gamma_H1^2 * (qeff' * qeff) * dt;
 
     % Voight-like notation to be compatible with mddMRI
     bt(i,:)     = B([1 5 9 2 3 6]) .* [1 1 1 sqrt(2) sqrt(2) sqrt(2)];
     mt(i,:)     = M([1 5 9 2 3 6]) .* [1 1 1 sqrt(2) sqrt(2) sqrt(2)];
 
     % Exchange weighting
-%     gamma(i,:)  = gwf_to_tex(gwf, rf, dt);
+    gamma(i,:)  = gwf_to_tex(gwf, rf, dt);
 
     % Gradient moments
     gMom_0(i,:) = fwf_gwf_to_motion_enc(gwf, rf, dt, 0, 0);
@@ -61,7 +61,7 @@ xps.m_rank   = fwf_1x6_to_rank(xps.mt);
 
 xps.bm_shape = fwf_1x6_to_shape(xps.bt .* xps.mt .* 1./[1 1 1 sqrt(2) sqrt(2) sqrt(2)]);
 
-% xps.gamma    = gamma/3; % Using Arthur convention
+xps.gamma    = gamma/3; % Using Arthur convention
 
 xps.gMom_0   = gMom_0;
 xps.gMom_1   = gMom_1;
--- common/gwf_tools/fwf_gwf_force_balance.m
+++ common/gwf_tools/fwf_gwf_force_balance.m
@@ -1,8 +1,12 @@
-function [gwf, rf, dt] = fwf_gwf_force_balance(gwf, rf, dt)
-% function [gwf, rf, dt] = fwf_gwf_force_balance(gwf, rf, dt)
+function [gwf, rf, dt] = fwf_gwf_force_balance(gwf, rf, dt, verbose)
+% function [gwf, rf, dt] = fwf_gwf_force_balance(gwf, rf, dt, verbose)
 % By FSz
 % Function forces the zeroth moment to be zero given that this can be
 % achieved by scaling pre or post wf.
+
+if nargin < 4
+    verbose = 0;
+end
 
 % Convert to effective waveform
 eg = gwf.*rf;
@@ -11,7 +15,7 @@ eg = gwf.*rf;
 m0 = sum(eg,1);
 
 for i = 1:numel(m0)
-    
+
     if m0(i)>0
         ind = eg(:,i)>0;
     elseif m0(i)<0
@@ -19,22 +23,24 @@ for i = 1:numel(m0)
     else
         ind = [];
     end
-    
+
     % potential for change
     pot = sum(eg(ind,i));
-    
+
     % multiplication factor
     fac = 1-abs(m0(i)/pot);
-    
+
     % Adjust appropriate elements
     eg(ind,i) = eg(ind,i) * fac;
-    
-    if abs(1-fac) > 0.01
-        warning(['Correction factor is larger than 1%! value for axis ' num2str(i) ' is ' num2str(fac) '!'])
-    end
-    
-    if abs(1-fac) > 0.05
-        warning(['Correction factor is larger than 5%! value for axis ' num2str(i) ' is ' num2str(fac) '!'])
+
+    if verbose
+        if abs(1-fac) > 0.01
+            warning(['Correction factor is larger than 1%! value for axis ' num2str(i) ' is ' num2str(fac) '!'])
+        end
+
+        if abs(1-fac) > 0.05
+            warning(['Correction factor is larger than 5%! value for axis ' num2str(i) ' is ' num2str(fac) '!'])
+        end
     end
 end
 
