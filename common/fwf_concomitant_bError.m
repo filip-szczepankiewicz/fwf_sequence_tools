464f11fb3014035981c5d7339f8abb5e9bf4d099
FilipSz <filip.szczepankiewicz@gmail.com>
2026-02-15 19:48:59 +0100

    changed file structure and names

--- common/fwf_concomitant_bError.m
+++
@@ -1,5 +0,0 @@
-function bError = fwf_concomitant_bError(c, radius, gl, bl)
-% function bError = fwf_concomitant_bError(c, radius, gl, bl)
-
-x = radius*gl'./bl;
-bError = c(1)*x + c(2)*x.^2; % In percent
